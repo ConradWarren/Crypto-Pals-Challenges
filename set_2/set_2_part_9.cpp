f65268de8e442ec11be52d5f30ef353623aeb464
ConradWarren <conrad_warren11@outlook.com>
2024-01-11 19:25:14 -0700

    Re-facotred set 2 part 9

---
+++ set_2/set_2_part_9.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <cstring>
+
+void Print_Byte_String(const std::string& byte_string){
+	
+	for(int i = 0; i<byte_string.size(); i++){
+		
+		if(std::isalnum(byte_string[i]) || std::isspace(byte_string[i]) || std::ispunct(byte_string[i])){
+			std::cout<<byte_string[i];
+		}else{
+			int num = (((int)byte_string[i]) >> 4);
+			std::cout<<"\\x"<<(char)((num > 9) ? 'a' + num - 10 : '0' + num);
+			num = ((int)byte_string[i]) & 0xf;
+			std::cout<< (char)((num > 9) ? 'a' + num - 10 : '0' + num);
+		}
+	}
+	
+	std::cout<<'\n';
+}
+
+std::string PKCS_7(const std::string& input, int block_size){
+	
+	if(input.size() >= block_size){
+		return input;
+	}
+	
+	char padding = (char)(block_size - input.size());
+	std::string result(block_size, padding);
+	
+	memcpy(result.data(), input.data(), input.size());
+	
+	return result;
+}
+
+int main(int argc, char** argv){
+	
+	if(argc > 1){
+		std::string padded_byte_string = PKCS_7(argv[1], 20);
+		Print_Byte_String(padded_byte_string);
+	}
+
+	return 0;
+}
